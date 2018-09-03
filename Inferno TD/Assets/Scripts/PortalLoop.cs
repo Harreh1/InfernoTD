b195a6966ea724fd2738f4d031abab45818626ed
Harrison Lucas <harry.l123@hotmail.com>
2018-09-04 08:45:59 +1000

    Work so far

---
+++ Inferno TD/Assets/Scripts/PortalLoop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalLoop : MonoBehaviour {
+
+    private SpriteRenderer sp;
+    //Variable to check whether value should be added or subracted from RGB. 
+    //1 for True
+    //0 for False
+    private int adding;
+	// Use this for initialization
+	void Start () {
+        sp = gameObject.GetComponent<SpriteRenderer>();
+        adding = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Color c = sp.color;
+        if (c.g > 1)
+        {
+            adding = 0;
+        }
+        if (c.g < 0)
+        {
+            adding = 1;
+        }
+        if(adding == 1)
+        {
+            c.g+=0.01f;
+        }
+        if(adding == 0)
+        {
+            c.g-=0.01f;
+        }
+        sp.color = c;
+    }
+}
